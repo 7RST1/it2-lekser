887c1612d10fbb73adcb64abdd1d67b15621789e
Syver Stensholt <sssandum@gmail.com>
2020-09-02 14:13:38 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,7 +1,11 @@
 var button = document.getElementById("button");
 button.addEventListener("click", () => {
     const name = prompt("Hva heter du?");
-    if (name) {
+    if (name && name.length < 20) {
         document.getElementById("loginmessage").textContent = `Logget inn som ${name}`
+    } else if (name.length >= 20) {
+        alert(`Navnet ditt er for langt! (${name.length - 19} ${name.length - 19 == 1 ? "symbol" : "symboler"} for langt)`)
+    } else if (!name) {
+        alert("Navnet ditt kan ikke være ingenting!")
     }
 });
